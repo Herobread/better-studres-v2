5f89536867ad0066d6cf84dacc82df15de79ea32
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-06-13 23:07:25 +0100

    exclude smooth nav urls for non folders

--- src/components/router/Link.tsx
+++ src/components/router/Link.tsx
@@ -1,8 +1,17 @@
 import { AnchorHTMLAttributes, MouseEvent, forwardRef } from "react"
 import useSmoothRouter from "./useSmoothRouter"
+import { BASE_URL, checkIfStringMatchesStringPatterns } from "@src/content/versionControl"
+import redirect from "@src/lib/redirect"
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+const escapedBaseUrl = BASE_URL.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+const EXCLUDED_SMOOTH_NAVIGATION_HREF_PATTERNS = [
+    new RegExp(`^${escapedBaseUrl}$`, 'i'), // Exact match for BASE_URL
+    new RegExp(`^${escapedBaseUrl}.*\\.[^/]+$`, 'i') // Match URLs with a dot in the final segment
+];
 
 const Link = forwardRef<HTMLAnchorElement, LinkProps>(({ ...props }, ref) => {
     const { href } = props
@@ -13,6 +22,13 @@ const Link = forwardRef<HTMLAnchorElement, LinkProps>(({ ...props }, ref) => {
         e.preventDefault()
 
         if (!href) {
+            return
+        }
+
+        const isExcluded = checkIfStringMatchesStringPatterns(href, EXCLUDED_SMOOTH_NAVIGATION_HREF_PATTERNS)
+        if (isExcluded) {
+            console.log('excluded: ' + href)
+            redirect(href, 'userClick')
             return
         }
 
