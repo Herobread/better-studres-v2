4b9e844bf1ec8ea0c84554ff6d0ca6b415ccfad8
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-06-21 16:59:55 +0100

    fix popstate with fake folders

--- src/features/router/SmoothRouterListener.tsx
+++ src/features/router/SmoothRouterListener.tsx
@@ -1,18 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import useSmoothRouter from "./useSmoothRouter"
+import { PageData } from "../parser"
+import { redirect } from "./redirect"
 
 export function SmoothRouterListener() {
     const { navigateToPage } = useSmoothRouter()
+    const lastStateRef = useRef<PageData>(history.state)
 
     useEffect(() => {
         const handlePopState = (e: PopStateEvent) => {
             e.preventDefault()
+
+            const lastState = lastStateRef.current
+
+            if (lastState.type === "unknown") {
+                redirect(window.location.toString())
+            }
+
+            lastStateRef.current = history.state
 
             navigateToPage(window.location.toString(), e.state)
         }
 
         window.addEventListener("popstate", handlePopState)
 
+        // Cleanup listener on component unmount
         return () => {
             window.removeEventListener("popstate", handlePopState)
         }
