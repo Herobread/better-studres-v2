9379ce497efd6d288821edbf5800499feb4fea7e
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-06-22 22:35:17 +0100

    link button and functionality

--- src/features/extensionToggle/extensionState.ts
+++ src/features/extensionToggle/extensionState.ts
@@ -1,7 +1,6 @@
 export const EXTENSION_STATE_STORAGE_KEY = "extension-state"
 
 export async function setExtensionState(isOn: boolean) {
-    console.log("setting " + isOn)
     await chrome.storage.local.set({
         [EXTENSION_STATE_STORAGE_KEY]: isOn,
     })
@@ -11,7 +10,6 @@ export async function getExtensionState() {
     const isOnObject = await chrome.storage.local.get(
         EXTENSION_STATE_STORAGE_KEY
     )
-    console.log(isOnObject)
 
     const isOn = isOnObject[EXTENSION_STATE_STORAGE_KEY]
 
