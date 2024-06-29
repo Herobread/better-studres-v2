3ac75346dc9c83b67a5dac3519529b49935d974d
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-06-29 20:17:01 +0100

    add fetchFileBlob function

---
+++ src/features/fileDownload/parseDocumentFromText.ts
@@ -0,0 +1,9 @@
+export async function parseDocumentFromText(htmlText: string) {
+    const parser = new DOMParser()
+    const document = parser.parseFromString(
+        htmlText,
+        "text/html"
+    )
+
+    return document
+}
