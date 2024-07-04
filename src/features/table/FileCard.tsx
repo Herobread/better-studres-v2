6857ed9b842e097d6115bf1c69bc6b68853f5bfd
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-07-04 21:56:21 +0100

    move dialog to table context

--- src/features/table/FileCard.tsx
+++ src/features/table/FileCard.tsx
@@ -3,14 +3,14 @@ import DefaultFileCard, { DefaultFileCardProps } from "./DefaultFileCard"
 import { ImageCard } from "./ImageCard"
 import { ConfigContext } from "@src/features/config"
 
-export default function FileCard({ fileLink }: DefaultFileCardProps) {
+export default function FileCard({ fileLink, ...props }: DefaultFileCardProps) {
     const { isImage } = fileLink
 
     const { imagePreviewOnHover } = useContext(ConfigContext)
 
     if (isImage && imagePreviewOnHover) {
-        return <ImageCard fileLink={fileLink} />
+        return <ImageCard fileLink={fileLink} {...props} />
     }
 
-    return <DefaultFileCard fileLink={fileLink} />
+    return <DefaultFileCard fileLink={fileLink} {...props} />
 }
