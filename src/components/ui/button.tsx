1e7250433458f81512f795d224edb0efa5e17160
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-07-04 22:30:44 +0100

    update destructive ghost button css

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
                     "bg-secondary text-secondary-foreground hover:bg-secondary/80",
                 ghost: "hover:bg-accent hover:text-accent-foreground",
                 destructiveGhost:
-                    "text-destructive-foreground hover:bg-destructive/90 hover:text-destructive-foreground/90",
+                    "text-destructive hover:text-destructive-foreground hover:bg-destructive/90",
                 link: "text-primary underline-offset-4 hover:underline",
             },
             size: {
