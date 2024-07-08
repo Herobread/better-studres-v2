ad04a3fe8ff21a9560568c5a6f57efb14eb39e70
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-07-08 10:31:43 +0100

    refactor quicklink dialog to use nicemodal

--- src/features/quickAccess/QuickLinks.tsx
+++ src/features/quickAccess/QuickLinks.tsx
@@ -1,3 +1,4 @@
+import NiceModal from "@ebay/nice-modal-react"
 import { useQuery } from "@tanstack/react-query"
 import {
     ArrowLeftIcon,
@@ -14,15 +15,14 @@ import {
     ContextMenuSeparator,
     ContextMenuTrigger,
 } from "../../components/ui/context-menu"
-import { Dialog, DialogContent } from "../../components/ui/dialog"
 import {
     Popover,
     PopoverContent,
     PopoverTrigger,
 } from "../../components/ui/popover"
+import EditQuickLinkDialog from "../dialogs/EditQuickLinkDialog"
 import CopyPathMenuItem from "../shared/commands/CopyPathMenuItem"
 import AddQuickLinkForm from "./AddQuickLinkForm"
-import EditQuickLinkForm from "./EditQuickLinkForm"
 import QuickLinkButton from "./QuickLinkButton"
 import { QuickLinkCard } from "./QuickLinkCard"
 import { QuickLinkContainer } from "./QuickLinkContainer"
@@ -35,19 +35,20 @@ import { generateQuickLinkInfo } from "./generateQuickLinkInfo"
 
 export function QuickLinks() {
     const [isOpen, setIsOpen] = useState(false)
-    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
-    const [editLinkId, setEditLinkId] = useState(-1)
 
     const { data: quickLinks, refetch } = useQuery({
         queryKey: ["quicklinks"],
         queryFn: getQuickLinks,
     })
 
-    const quickLinkToEdit = quickLinks?.find((link) => link.id === editLinkId)
-
     const currentUrl = window.location.toString()
     const { href: currentQuickLinkHref, name: currentQuickLinkName } =
         generateQuickLinkInfo(currentUrl)
+
+    const handleEditQuickLink = (id: number) => {
+        const quickLinkToEdit = quickLinks?.find((link) => link.id === id)
+        NiceModal.show(EditQuickLinkDialog, { quickLink: quickLinkToEdit })
+    }
 
     const handleQuickLinksUpdated = async () => {
         await refetch()
@@ -92,10 +93,9 @@ export function QuickLinks() {
                             </ContextMenuTrigger>
                             <ContextMenuContent>
                                 <ContextMenuItem
-                                    onSelect={() => {
-                                        setEditLinkId(quickLink.id)
-                                        setIsEditDialogOpen(true)
-                                    }}
+                                    onSelect={() =>
+                                        handleEditQuickLink(quickLink.id)
+                                    }
                                 >
                                     <Edit2Icon /> Edit link
                                 </ContextMenuItem>
@@ -136,23 +136,6 @@ export function QuickLinks() {
                     <p>Add any URL to quick access by clicking the button</p>
                 </p>
             )}
-
-            <Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
-                <DialogContent>
-                    {quickLinkToEdit ? (
-                        <EditQuickLinkForm
-                            quickLink={quickLinkToEdit}
-                            afterSubmit={() => {
-                                setEditLinkId(-1)
-                                setIsEditDialogOpen(false)
-                                handleQuickLinksUpdated()
-                            }}
-                        />
-                    ) : (
-                        <p>Quick link not found</p>
-                    )}
-                </DialogContent>
-            </Dialog>
         </QuickLinkContainer>
     )
 }
