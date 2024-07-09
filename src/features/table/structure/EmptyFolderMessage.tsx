4ee5205cc03648378ce90794daac9a31620f1b14
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-07-09 17:00:08 +0100

    folder empty message

---
+++ src/features/table/structure/EmptyFolderMessage.tsx
@@ -0,0 +1,7 @@
+export function EmptyFolderMessage() {
+    return (
+        <p className="text-center text-sm text-muted-foreground">
+            This folder is empty.
+        </p>
+    )
+}
--- src/features/table/structure/Table.tsx
+++ src/features/table/structure/Table.tsx
@@ -1,6 +1,7 @@
 import { FullFileLink } from "@src/features/parser"
 import { PageStateContext } from "@src/features/router/PageStateContext"
 import FileCard from "@src/features/table/fileCard/FileCard"
+import { EmptyFolderMessage } from "@src/features/table/structure/EmptyFolderMessage"
 import { SortLinks } from "@src/types/pageContentTypes"
 import { useContext } from "react"
 import TableHeader from "./TableHeader"
@@ -21,17 +22,22 @@ export function Table({ fileLinks, sortLinks }: TableProps) {
     let state = isLoading ? "closed" : "open"
 
     return (
-        <div
-            data-state={state}
-            className="grid grid-cols-[50px_auto_4fr_max-content_max-content_3fr] gap-3 
-          transition-opacity data-[state=open]:animate-in data-[state=closed]:animate-out
-          data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 
-          "
-        >
-            <TableHeader sortLinks={sortLinks} />
-            {fileLinks.map((fileLink) => {
-                return <FileCard fileLink={fileLink} key={fileLink.fileKey} />
-            })}
-        </div>
+        <>
+            <div
+                data-state={state}
+                className="grid grid-cols-[50px_auto_4fr_max-content_max-content_3fr] gap-3 
+            transition-opacity data-[state=open]:animate-in data-[state=closed]:animate-out
+            data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 
+            "
+            >
+                <TableHeader sortLinks={sortLinks} />
+                {fileLinks.map((fileLink) => {
+                    return (
+                        <FileCard fileLink={fileLink} key={fileLink.fileKey} />
+                    )
+                })}
+            </div>
+            {fileLinks.length === 1 && <EmptyFolderMessage />}
+        </>
     )
 }
