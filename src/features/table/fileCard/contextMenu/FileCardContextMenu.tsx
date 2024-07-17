7d9c50bbb5b3e22b10a99397329274f83e5e3c8b
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-07-17 19:05:36 +0100

    add go to module root for parent dir context menu

--- src/features/table/fileCard/contextMenu/FileCardContextMenu.tsx
+++ src/features/table/fileCard/contextMenu/FileCardContextMenu.tsx
@@ -3,12 +3,16 @@ import {
     ContextMenuItem,
     ContextMenuSeparator,
 } from "@src/components/ui/context-menu"
-import { BASE_URL } from "@src/features/files"
+import {
+    BASE_URL,
+    convertUrlSegmentsToUrl,
+    extractUrlSegments,
+} from "@src/features/files"
 import { FullFileLink } from "@src/features/parser"
 import { redirect } from "@src/features/router"
 import CopyPathMenuItem from "@src/features/shared/contextMenuItems/CopyPathMenuItem"
 import { AddNoteContextMenuItem } from "@src/features/table/fileCard/contextMenu/AddNoteContextMenuItem"
-import { FolderIcon } from "lucide-react"
+import { FolderIcon, FolderRootIcon } from "lucide-react"
 import { forwardRef } from "react"
 import DownloadFileMenuItem from "./DownloadFileMenuItem"
 import { MenuLabel } from "./MenuLabel"
@@ -26,10 +30,18 @@ const FileCardContextMenuContent = forwardRef<
     const isParentDirectory = fileLink.fullName === "Parent Directory"
 
     if (isParentDirectory) {
+        const currentUrl = location.href
+        const urlSegments = extractUrlSegments(currentUrl)
+        const moduleRootUrl = convertUrlSegmentsToUrl([urlSegments[0]])
+
         return (
             <ContextMenuContent ref={ref}>
                 <ContextMenuItem onClick={() => redirect(BASE_URL)}>
-                    <FolderIcon className="h-4 w-4" /> Go to Root
+                    <FolderIcon className="h-4 w-4" /> Go to root
+                </ContextMenuItem>
+                <ContextMenuItem onClick={() => redirect(moduleRootUrl)}>
+                    <FolderRootIcon className="h-4 w-4" />
+                    Go to module root
                 </ContextMenuItem>
             </ContextMenuContent>
         )
