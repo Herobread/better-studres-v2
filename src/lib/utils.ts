5b594545bfdd2149efd4603613d93e2fb7714221
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-07-22 19:45:28 +0100

    fix memory leak in config

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -33,3 +33,29 @@ export function downloadFile(href: string, filename: string): void {
 export function endsWith(str: string, ends: string[]) {
     return ends.some((end) => str.endsWith(end))
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function deepEqual(obj1: any, obj2: any) {
+    if (obj1 === obj2) {
+        return true
+    }
+    if (
+        typeof obj1 !== "object" ||
+        typeof obj2 !== "object" ||
+        obj1 == null ||
+        obj2 == null
+    ) {
+        return false
+    }
+    const keys1 = Object.keys(obj1)
+    const keys2 = Object.keys(obj2)
+    if (keys1.length !== keys2.length) {
+        return false
+    }
+    for (const key of keys1) {
+        if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+            return false
+        }
+    }
+    return true
+}
