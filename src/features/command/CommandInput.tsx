c648ea0647333c0a8032eb87a16a0f795ece8846
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-08-03 12:11:30 +0100

    add tab support for command input

--- src/features/command/CommandInput.tsx
+++ src/features/command/CommandInput.tsx
@@ -7,8 +7,8 @@ interface CommandInputProps {
 export default function CommandInput({ onSelect }: CommandInputProps) {
     return (
         <button
-            onMouseDown={onSelect}
-            className="flex items-center gap-1 rounded-xl p-1 text-muted-foreground outline outline-2 outline-muted"
+            onClick={onSelect}
+            className="flex items-center gap-1 rounded-xl p-1 text-muted-foreground outline outline-2 outline-muted ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
         >
             <SearchIcon />
             <p className="flex-grow">Search visited folders or run commands</p>
