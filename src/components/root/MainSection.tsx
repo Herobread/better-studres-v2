866f10a2a017e80742272c0bd10b4374ae1e7154
YehorBoiar <boyaregor1@gmail.com>
2024-08-18 15:50:23 +0100

    Change link appearence

--- src/components/root/MainSection.tsx
+++ src/components/root/MainSection.tsx
@@ -1,4 +1,6 @@
+import { getModuleEmoji } from '@src/features/contentEnhancers/emoji/modules';
 import { RootContent } from '@src/features/parser';
+import { QuickLinkLink } from '@src/features/quickLinks/components/quickLink/QuickLinkLink';
 import React from 'react';
 
 
@@ -6,50 +8,68 @@ interface MainSectionProps {
   content: RootContent;
 }
 
-const MainSection: React.FC<MainSectionProps> = ( { content }) => (
-  <section className="main py-10 bg-gray-50">
-    <div className="container mx-auto">
+const MainSection: React.FC<MainSectionProps> = ({ content }) => (
+  <div className="main py-10 bg-gray-50">
+
       <p className="text-lg text-gray-700">
         Student Resources (StudRes) is a repository of teaching materials, principally for students enrolled on Computer Science modules. Staff may add or remove items to this library; students may read, copy or download them.
       </p>
-      <p>
-        <a href="https://wiki.cs.st-andrews.ac.uk/index.php?title=StudRes" className="text-blue-600 underline">More about Student Resources</a>
-      </p>
-
+      <QuickLinkLink
+        quickLink={{
+          href: "https://wiki.cs.st-andrews.ac.uk/index.php?title=StudRes",
+          icon: getModuleEmoji("More about Student Resources"),
+          id: 0,
+          name: "More about Student Resources",
+        }}
+      />
       <h2 className="text-2xl font-semibold mt-8">Level-based resources</h2>
 
       <h3 className="text-xl font-semibold mt-4">Postgraduate research students</h3>
-      <p>
-        <a href="https://studres.cs.st-andrews.ac.uk/PGR" className="text-blue-600 underline">
-          Materials relevant to PGR students
-        </a>
-      </p>
+      
+      <QuickLinkLink
+        quickLink={{
+          href: "https://studres.cs.st-andrews.ac.uk/PGR",
+          icon: getModuleEmoji("Materials relevant to PGR students"),
+          id: 0,
+          name: "Materials relevant to PGR students",
+        }}
+      />
+    
 
       <h3 className="text-xl font-semibold mt-4">Taught students</h3>
-      <p>Materials relevant to students on taught programmes.</p>
+      <p className="text-gray-700">Materials relevant to students on taught programmes.</p>
 
       <div className="flex flex-wrap mt-4">
-        {content.taught_students.map(({ code, url }) => (
-          <a
-            key={code}
-            href={url}
-            className="btn btn-primary btn-lg bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-md px-4 py-2 mx-2 my-2"
-          >
-            {code}
-          </a>
-        ))}
+      {content.taught_students.map((module, index) => (
+        <QuickLinkLink
+          quickLink={{
+            href: module.url,
+            icon: getModuleEmoji(module.url.split('/').pop() || ""),
+            id: index, 
+            name: module.url.split('/').pop() || "",
+          }}
+        />
+      ))}
       </div>
 
-      <h2 className="text-2xl font-semibold mt-8">Modules</h2>
-      <div className="flex flex-wrap mt-4">
-        {content.modules.map((module, index) => (
-          <a key={index} href={module.url} className="text-blue-600 underline mx-2 my-2">
-            {module.code}
-          </a>
-        ))}
-      </div>
+
+    <h2 className="text-2xl font-semibold mt-8">Modules</h2>
+
+    <div className="flex flex-wrap mt-4">
+      {content.modules.map((module, index) => (
+
+        <QuickLinkLink
+          quickLink={{
+            href: module.url,
+            icon: getModuleEmoji(module.url),
+            id: index,
+            name: module.code,
+          }}
+        />
+      ))}
     </div>
-  </section>
+
+  </div>
 );
 
 export default MainSection;
