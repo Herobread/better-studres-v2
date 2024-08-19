99c2c1354c2981893c56619c8da6cd3b0a2733ba
YehorBoiar <boyaregor1@gmail.com>
2024-08-19 10:05:41 +0100

    Added more elements on page

--- src/pages/content/pages/RootPage.tsx
+++ src/pages/content/pages/RootPage.tsx
@@ -1,11 +1,17 @@
 import NiceModal from "@ebay/nice-modal-react";
+import CompactLayout from "@src/components/layouts/CompactLayout";
+import MainLayout from "@src/components/layouts/MainLayout";
 import WideLayout from "@src/components/layouts/WideLayout";
 import MainSection from "@src/components/root/MainSection";
+import { ThemeToggle } from "@src/components/ui/theme-toggle";
 import { SkipToMainContent } from "@src/features/accessibility/SkipToMainContent";
 import CommandInput from "@src/features/command/CommandInput";
 import { CommandsShortcutMount } from "@src/features/command/CommandsShortcutMount";
 import { ConfigContext } from "@src/features/config";
+import SubheaderBreadcrumbs from "@src/features/header/SubheaderBreadCrumbs";
 import { RootContent } from "@src/features/parser";
+import { AddQuickLinkButton, QuickLinkContainer, QuickLinkList } from "@src/features/quickLinks/components";
+import { EmptyQuickLinksMessage } from "@src/features/quickLinks/components/EmptyQuickLinksMessage";
 import CommandsDialog from "@src/features/shared/dialogs/CommandsDialog";
 import { useContext } from "react";
 
@@ -14,8 +20,7 @@ interface RootProps {
 }
 
 export function RootPage({ content }: RootProps) {
-    const { showCommandButton } = useContext(ConfigContext);
-    const modules = content
+    const { showCommandButton, showQuickLinks } = useContext(ConfigContext);
     const handleCommandActivation = () => {
         NiceModal.show(CommandsDialog);
     };
@@ -24,12 +29,27 @@ export function RootPage({ content }: RootProps) {
         <div className="flex flex-col min-h-screen bg-background py-2 text-foreground">
             <CommandsShortcutMount />
             {showCommandButton && (
-            <WideLayout>
+                <WideLayout>
                     <CommandInput onSelect={handleCommandActivation} />
-            </WideLayout>
+                </WideLayout>
             )}
-            <SkipToMainContent />    
-            <MainSection content={content}/>
+            <SkipToMainContent />
+            <MainLayout>
+                <CompactLayout>
+                    <div className="grid grid-cols-[1fr_max-content]">
+                        <ThemeToggle />
+                    </div>
+                    <SubheaderBreadcrumbs />
+                </CompactLayout>
+                {showQuickLinks && (
+                    <QuickLinkContainer>
+                        <AddQuickLinkButton />
+                        <QuickLinkList />
+                        <EmptyQuickLinksMessage />
+                    </QuickLinkContainer>
+                )}
+                <MainSection content={content} />
+            </MainLayout>
 
         </div>
     );
