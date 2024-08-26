db1a6de320e464f848847b3125b901b937718a40
YehorBoiar <boyaregor1@gmail.com>
2024-08-26 10:06:52 +0100

    Added RootHeader

--- src/pages/content/pages/RootPage.tsx
+++ src/pages/content/pages/RootPage.tsx
@@ -1,33 +1,36 @@
-import NiceModal from "@ebay/nice-modal-react";
-import CompactLayout from "@src/components/layouts/CompactLayout";
-import MainLayout from "@src/components/layouts/MainLayout";
-import WideLayout from "@src/components/layouts/WideLayout";
-import MainSection from "@src/features/root/MainSection";
-import H1 from "@src/components/typography/H1";
-import { ThemeToggle } from "@src/components/ui/theme-toggle";
-import { SkipToMainContent } from "@src/features/accessibility/SkipToMainContent";
-import CommandInput from "@src/features/command/CommandInput";
-import { CommandsShortcutMount } from "@src/features/command/CommandsShortcutMount";
-import { ConfigContext } from "@src/features/config";
-import SubheaderBreadcrumbs from "@src/features/header/SubheaderBreadCrumbs";
-import { RootContent } from "@src/features/parser";
-import { AddQuickLinkButton, QuickLinkContainer, QuickLinkList } from "@src/features/quickLinks/components";
-import { EmptyQuickLinksMessage } from "@src/features/quickLinks/components/EmptyQuickLinksMessage";
-import CommandsDialog from "@src/features/shared/dialogs/CommandsDialog";
-import { useContext } from "react";
+import NiceModal from "@ebay/nice-modal-react"
+import CompactLayout from "@src/components/layouts/CompactLayout"
+import MainLayout from "@src/components/layouts/MainLayout"
+import WideLayout from "@src/components/layouts/WideLayout"
+import MainSection from "@src/features/root/MainSection"
+import RootHeader from "@src/features/header/RootHeader"
+import { SkipToMainContent } from "@src/features/accessibility/SkipToMainContent"
+import CommandInput from "@src/features/command/CommandInput"
+import { CommandsShortcutMount } from "@src/features/command/CommandsShortcutMount"
+import { ConfigContext } from "@src/features/config"
+import { RootContent } from "@src/features/parser"
+import {
+    AddQuickLinkButton,
+    QuickLinkContainer,
+    QuickLinkList,
+} from "@src/features/quickLinks/components"
+import { EmptyQuickLinksMessage } from "@src/features/quickLinks/components/EmptyQuickLinksMessage"
+import CommandsDialog from "@src/features/shared/dialogs/CommandsDialog"
+import { useContext } from "react"
+import { ThemeToggle } from "@src/components/ui/theme-toggle"
 
 interface RootProps {
     content: RootContent
 }
 
 export function RootPage({ content }: RootProps) {
-    const { showCommandButton, showQuickLinks } = useContext(ConfigContext);
+    const { showCommandButton, showQuickLinks } = useContext(ConfigContext)
     const handleCommandActivation = () => {
-        NiceModal.show(CommandsDialog);
-    };
+        NiceModal.show(CommandsDialog)
+    }
 
     return (
-        <div className="flex flex-col min-h-screen bg-background py-2 text-foreground">
+        <div className="flex min-h-screen flex-col bg-background py-2 text-foreground">
             <CommandsShortcutMount />
             {showCommandButton && (
                 <WideLayout>
@@ -38,10 +41,9 @@ export function RootPage({ content }: RootProps) {
             <MainLayout>
                 <CompactLayout>
                     <div className="grid grid-cols-[1fr_max-content]">
-                        <H1>Student Resources</H1>
+                        <RootHeader />
                         <ThemeToggle />
                     </div>
-                    <SubheaderBreadcrumbs />
                 </CompactLayout>
                 {showQuickLinks && (
                     <QuickLinkContainer>
@@ -52,9 +54,8 @@ export function RootPage({ content }: RootProps) {
                 )}
                 <MainSection content={content} />
             </MainLayout>
-
         </div>
-    );
+    )
 }
 
-export default RootPage;
+export default RootPage
