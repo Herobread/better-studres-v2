65982f9e0452ff709ad2d0126cae06df7fafca6d
YehorBoiar <boyaregor1@gmail.com>
2024-08-27 11:26:29 +0100

    Small corrections

--- src/features/accessibility/SkipToMainContent.tsx
+++ src/features/accessibility/SkipToMainContent.tsx
@@ -6,7 +6,7 @@ export function SkipToMainContent() {
     return (
         <button
             onClick={handleSkip}
-            className="sr-only focus:not-sr-only"
+            className="sr-only p-2 focus:not-sr-only focus:absolute focus:left-0 focus:top-0 focus:z-50 focus:h-auto focus:w-auto focus:bg-background"
         >
             Skip to main content
         </button>
