16bbffc01aa7b717a6ff69b2c0bd617ddeed721c
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-08-28 12:36:11 +0100

    use H3 instead of h3

--- src/features/root/MainSectionModules.tsx
+++ src/features/root/MainSectionModules.tsx
@@ -1,16 +1,19 @@
-import { QuickLinkContainer } from "@src/features/quickLinks/components"
-import { QuickLinkLink } from "@src/features/quickLinks/components/quickLink/QuickLinkLink"
+import H3 from "@src/components/typography/H3"
 import { getModuleEmoji } from "@src/features/contentEnhancers/emoji/modules"
 import { ModuleContent } from "@src/features/parser/root/parseRootPageContent"
+import { QuickLinkContainer } from "@src/features/quickLinks/components"
+import { QuickLinkLink } from "@src/features/quickLinks/components/quickLink/QuickLinkLink"
 
 interface MainSectionModulesProps {
     modulesGroups: ModuleContent[][]
 }
 
-export default function MainSectionModules({ modulesGroups }: MainSectionModulesProps) {
+export default function MainSectionModules({
+    modulesGroups,
+}: MainSectionModulesProps) {
     return (
         <div className="space-y-4">
-            <h3>Modules</h3>
+            <H3>Modules</H3>
             {modulesGroups.map((modules, groupIndex) => (
                 <QuickLinkContainer key={groupIndex}>
                     {modules.map((module, index) => (
