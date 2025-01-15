e19ec9deea9b390bb0bcb3f2ba8d02e48e661e9d
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-01-15 22:27:24 +0000

    extract <Command> out of CommandDialog for pages support

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -16,6 +16,7 @@ const Command = React.forwardRef<
         ref={ref}
         className={cn(
             "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
+            "[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5",
             className
         )}
         {...props}
@@ -32,9 +33,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
     return (
         <Dialog {...props}>
             <DialogContent className="__better_studres_theme_root _tailwind_preflight_reset overflow-hidden p-0 shadow-lg">
-                <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-                    {children}
-                </Command>
+                {children}
             </DialogContent>
         </Dialog>
     )
