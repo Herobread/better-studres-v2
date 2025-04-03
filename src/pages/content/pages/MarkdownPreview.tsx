8f418f4314abbdd13735f057228537bf389dd0da
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-04-03 15:15:28 +0100

    replace bg color

--- src/pages/content/pages/MarkdownPreview.tsx
+++ src/pages/content/pages/MarkdownPreview.tsx
@@ -9,8 +9,47 @@ import { FilePreviewToolbar } from "@src/features/tools/FilePreviewToolbar"
 import { cn } from "@src/lib/utils"
 import { highlighter } from "@src/pages/content"
 import Markdown from "react-markdown"
+import githubDark from "shiki/themes/github-dark.mjs"
+
+const customTheme = { ...githubDark }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function replaceColors(theme: any, colorMap: { [key: string]: string }) {
+    if (!theme || !theme.colors) return
+
+    for (const key in theme.colors) {
+        if (colorMap[theme.colors[key]]) {
+            theme.colors[key] = colorMap[theme.colors[key]]
+        }
+    }
+
+    if (Array.isArray(theme.tokenColors)) {
+        for (const token of theme.tokenColors) {
+            if (
+                token.settings &&
+                token.settings.foreground &&
+                colorMap[token.settings.foreground]
+            ) {
+                token.settings.foreground = colorMap[token.settings.foreground]
+            }
+            if (
+                token.settings &&
+                token.settings.background &&
+                colorMap[token.settings.background]
+            ) {
+                token.settings.background = colorMap[token.settings.background]
+            }
+        }
+    }
+}
+
+const colorMap = {
+    "#24292e": "transparent",
+}
 
 export function MarkdownPreview({ content }: { content: FileContent }) {
+    replaceColors(customTheme, colorMap)
+
     return (
         <div className="min-h-screen bg-background text-foreground">
             <CommandsShortcutMount />
@@ -18,6 +57,7 @@ export function MarkdownPreview({ content }: { content: FileContent }) {
             <div className="prose mx-auto max-w-prose py-12 dark:prose-invert ">
                 <Markdown
                     components={{
+                        pre: ({ children }) => <>{children}</>,
                         code(props) {
                             const { children, className } = props
 
@@ -51,17 +91,22 @@ export function MarkdownPreview({ content }: { content: FileContent }) {
                                     extension as SupportedShikiFileFormats
                                 ] || "plaintext"
 
+                            console.log(customTheme)
+
                             const html = highlighter.codeToHtml(
                                 childrenString,
                                 {
                                     lang,
-                                    theme: "github-dark",
+                                    theme: customTheme,
                                 }
                             )
 
                             return (
-                                <code
-                                    className={cn(className, "not-prose")}
+                                <div
+                                    className={cn(
+                                        className,
+                                        "not-prose rounded-sm bg-muted p-2"
+                                    )}
                                     dangerouslySetInnerHTML={{ __html: html }}
                                 />
                             )
