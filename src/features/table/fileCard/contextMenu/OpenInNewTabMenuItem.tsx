ee52177810b4ac7ffe218671b706550325d3bc8d
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-05-23 13:45:26 +0100

    feat: add "Open in new tab" menu item to file card context menu

---
+++ src/features/table/fileCard/contextMenu/OpenInNewTabMenuItem.tsx
@@ -0,0 +1,14 @@
+import { ContextMenuItem } from "@src/components/ui/context-menu"
+import { ExternalLinkIcon } from "lucide-react"
+
+export function OpenInNewTabMenuItem({ href }: { href: string }) {
+    const onSelect = () => {
+        window.open(href, "_blank")
+    }
+
+    return (
+        <ContextMenuItem onSelect={onSelect}>
+            <ExternalLinkIcon className="h-4 w-4"/> Open in new tab
+        </ContextMenuItem>
+    )
+}
