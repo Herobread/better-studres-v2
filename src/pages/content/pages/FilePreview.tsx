7e1bacceb3211b77766de6d11aceda80f8e8e5ec
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-05-30 10:45:42 +0100

    feat: add line numbers

--- src/pages/content/pages/FilePreview.tsx
+++ src/pages/content/pages/FilePreview.tsx
@@ -8,6 +8,31 @@ import { FileBackButton } from "@src/features/tools/FileBackButton"
 import { FilePreviewToolbar } from "@src/features/tools/FilePreviewToolbar"
 import { highlighter } from "@src/pages/content"
 
+// The following styles are for line numbering.
+// This approach is used because CSS counters are
+// not directly supported by Tailwind CSS and shiki.js
+//
+// shiki.js generates HTML structure that requires
+// these specific CSS rules to target the code lines for numbering.
+//
+// https://github.com/shikijs/shiki/issues/3
+const codeLineNumberingStyles = `
+  .code-container code {
+    counter-reset: step;
+    counter-increment: step 0;
+  }
+
+  .code-container code .line::before {
+    content: counter(step);
+    counter-increment: step;
+    width: 1rem;
+    margin-right: 1.5rem;
+    display: inline-block;
+    text-align: right;
+    color: #9097A0;
+  }
+`
+
 export function FilePreview({ content }: { content: FileContent }) {
     const extension = (
         splitFileName(window.location.toString()).extension || ""
@@ -25,9 +50,10 @@ export function FilePreview({ content }: { content: FileContent }) {
         <>
             <CommandsShortcutMount />
             <FileBackButton />
+            <style>{codeLineNumberingStyles}</style>
             <div
                 dangerouslySetInnerHTML={{ __html: html }}
-                className="min-h-screen w-max min-w-full bg-[#24292e] p-4 pb-20"
+                className="code-container min-h-screen w-max min-w-full bg-[#24292e] p-4 pb-20"
             />
             <FilePreviewToolbar />
         </>
