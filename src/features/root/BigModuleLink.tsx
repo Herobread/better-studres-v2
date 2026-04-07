7cebb3e7c112f2e2d8038e19a2ad297c3ea753b0
Oleksii Nazarenko <alexeybread81@gmail.com>
2026-04-07 19:47:27 +0100

    chore: revert vertical

--- src/features/root/BigModuleLink.tsx
+++ src/features/root/BigModuleLink.tsx
@@ -14,7 +14,7 @@ export function BigModuleLink({ href, name }: { href: string; name: string }) {
             <ContextMenuTrigger asChild>
                 <Link
                     href={href}
-                    className="group flex cursor-pointer flex-col items-center justify-center gap-2 rounded-xl bg-background-layer-1 px-3 py-2 outline-none ring-offset-background transition-all hover:bg-accent focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+                    className="group flex cursor-pointer items-center justify-center gap-2 rounded-xl bg-background-layer-1 px-3 py-2 text-center outline-none ring-offset-background transition-all hover:bg-accent focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
                 >
                     <div>{icon}</div>
                     <div className="font-mono">{name}</div>
